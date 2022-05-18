6dbd1d3aff68367f3159e20e68997b54204261ab
Khyber Sen <kkysen@gmail.com>
2022-05-18 19:22:32 -0400

    Added `install.sh`, which just `deno install`s it.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -x
+
+deno install \
+    --allow-net=ziglang.org \
+    --allow-run=tar,unzip,zig \
+    --allow-env=HOME,USERPROFILE,PATH \
+    --allow-read \
+    --allow-write \
+    https://raw.githubusercontent.com/kkysen/zig-update/main/update-zig.ts \
+    "${@}"
