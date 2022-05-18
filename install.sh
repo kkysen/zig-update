79b87e0bc97f8786f1313adf5758ab32eb2878b8
Khyber Sen <kkysen@gmail.com>
2022-05-18 19:35:35 -0400

    Fixed `install.sh` to always reload the main script.

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 set -x
 
+URL=https://raw.githubusercontent.com/kkysen/zig-update/main/update-zig.ts
+
 deno install \
+    --reload="${URL}" \
+    --force \
     --allow-net=ziglang.org \
     --allow-run=tar,unzip,zig \
     --allow-env=HOME,USERPROFILE,PATH \
     --allow-read \
     --allow-write \
     "${@}" \
-    https://raw.githubusercontent.com/kkysen/zig-update/main/update-zig.ts
+    "${URL}"
